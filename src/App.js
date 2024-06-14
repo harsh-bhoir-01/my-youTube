e31e796a5330cb4dd0932ea71b50bcadcbcbfa27
omkar48 <omkar.damacloid@gmail.com>
2024-06-14 11:42:21 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-600">Hello Friends tar chala shuru karuya</h1>
+    </div>
+  );
+}
+
+export default App;
