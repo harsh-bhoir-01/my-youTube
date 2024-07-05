00bfe35462e74d4e445737085c2d433647f16e2e
harsh-bhoir-01 <harshbhoir1302@gmail.com>
2024-07-05 20:30:07 +0530

    Updated constant file

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -7,4 +7,4 @@ export const YOUTUBE_VIDEOS_API =
   AUTH_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
