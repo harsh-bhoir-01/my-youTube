7616830000cab6e203a290769aeeed70344ef69c
harsh-bhoir-01 <harshbhoir1302@gmail.com>
2024-07-06 12:03:54 +0530

    added button shadow

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ name }) => {
   return (
     <div>
-      <button className="px-3 py-1 mx-2 bg-gray-200 rounded-lg font-sans italic text-sm font-medium">
+      <button className=" hover:bg-black hover:text-white shadow-lg px-3 py-1 mx-2 bg-gray-200 rounded-lg font-sans italic text-sm font-medium">
         {name}
       </button>
     </div>
